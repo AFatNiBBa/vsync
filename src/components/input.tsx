01cc3c621530e6855910b72705c986455cdc4be2
Sean <seanalunni@gmail.com>
2022-05-28 18:49:59 +0200

    base

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -10,15 +10,23 @@ type Opts = {
     children?: import("solid-js").JSX.Element
 };
 
+declare const sync: import("../lib/synchronizer").default;
+
 export default function Input(props: Opts) {
     const id = `input-${ props.name }`;    
+
+    function onChanged(e) {
+        args[props.name] = e.target.value;  // Aggiorna i parametri dell'app
+        sync.send();                        // Invia i parametri al server (Altrimenti se il video era pausato non li mandava)
+    }
+
     return (
         <div class="form-group">
             <label for={ id }>
                 <h4> { props.label } { props.icon } </h4>
             </label>
             <br />
-            <input id={ id } name={ props.name } value={ args[props.name] } onChange={e => args[props.name] = e.target.value} placeholder="Vuoto" class="form-control" />
+            <input id={ id } name={ props.name } value={ args[props.name] } onChange={onChanged} placeholder="Vuoto" class="form-control" />
             <small class="form-text text-muted"> { props.children } </small>
         </div>
     );
