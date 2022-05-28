4c77729deb916dfd1b5eb174e460f3560d02334a
Sean <seanalunni@gmail.com>
2022-05-28 18:35:30 +0200

    fix3

--- src/index.js
+++ src/index.js
@@ -38,16 +38,14 @@ app.ws("/", socket => {
                 info = Room.migrate(info, socket, obj.room),
                 show = true;
 
-            console.log(">>>", info.pass, (obj.pass ||= null), lastPass, info.pass == obj.pass, info.pass == lastPass);
-
             // Cambio password
-            if (info.pass != (obj.pass ||= null))                      // Se la password della stanza è diversa da quella del pacchetto
-                if (info.pass == lastPass)                                      // Se la password della stanza era stata inserita corretttamente la scorsa volta
-                    info.pass = obj.pass,                            // Cambia la password della stanza
+            if (info.pass != (obj.pass ||= null))   // Se la password della stanza è diversa da quella del pacchetto
+                if (info.pass == lastPass)          // Se la password della stanza era stata inserita corretttamente la scorsa volta
+                    info.pass = obj.pass,           // Cambia la password della stanza
                     show = true;
                 else
-                    return;                                                     // Altrimenti, se la password non è corretta, esce
-            lastPass = obj.pass; // Aggiorna la password precedente, quest'ultima permette di mantenere i permessi d'amministratore per un ciclo dopo il cambio della password
+                    return;                         // Altrimenti, se la password non è corretta, esce
+            lastPass = obj.pass;                    // Aggiorna la password precedente, quest'ultima permette di mantenere i permessi d'amministratore per un ciclo dopo il cambio della password
             
             // Cambio stato pausa
             if (obj.paused != null && info.paused != obj.paused)
