ec8608f1e09c5f9d0061c69c2802bc7191e4ac15
Sean Alunni <70455845+AFatNiBBa@users.noreply.github.com>
2022-07-02 00:54:16 +0200

    Sincronizzazione parametri url dopo spostamento nella cronologia interna alla pagina

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,10 @@
 
-import { createEffect } from "solid-js";
+import { createEffect, untrack } from "solid-js";
 import { ReactiveSynchronizer } from './synchronizer';
+
+const params = [ "room", "link", "pass" ];
+export const url = new URL(globalThis.location.href);
+export const sync = globalThis.sync = new ReactiveSynchronizer(url.searchParams.get("room"), url.searchParams.get("pass"), url.searchParams.get("link"));
 
 export function shiftTab(template: TemplateStringsArray, ...args: any[]) {
     var out = "";
@@ -10,15 +14,19 @@ export function shiftTab(template: TemplateStringsArray, ...args: any[]) {
     return out.trim();
 }
 
-export const url = new URL(globalThis.location.href);
-export const sync = globalThis.sync = new ReactiveSynchronizer(url.searchParams.get("room"), url.searchParams.get("pass"), url.searchParams.get("link"));
-
 sync.room ??= ReactiveSynchronizer.uuid();
 sync.link ??= "https://server7.streamingaw.online/DDL/ANIME/SpyXFamilyAW/SpyXFamily_Ep_01_SUB_ITA.mp4"
 
 createEffect(() => {
-    for (const k of [ "room", "link", "pass" ])
+    for (const k of params)
         url.searchParams[sync[k] ? "set" : "delete"](k, sync[k]);    
     sync.send();
     history.pushState({}, "", url.href);
 });
+
+globalThis.onpopstate = globalThis.onpushstate = () => {
+    untrack(() => {
+        for (const k of params)
+            sync[k] = url.searchParams.get(k);
+    });
+};
