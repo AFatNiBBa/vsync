d80d740abca06605ce12e4c7dc1e6dbb0ffb1ab8
Sean <seanalunni@gmail.com>
2022-12-04 03:26:52 +0100

    boh

--- src/lib/puppeteer.sh
+++ src/lib/puppeteer.sh
@@ -1,6 +1,11 @@
 
 apt install nodejs
 apt install npm
+
+# Questi
 sudo apt-get update
 sudo apt-get install -y libgbm-dev
 sudo apt-get install libasound2
+
+# O questo
+sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev
