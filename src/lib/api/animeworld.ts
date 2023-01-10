f475f83a18d93215b46ff2ea8a642352720541b3
Sean <seanalunni@gmail.com>
2023-01-11 00:06:04 +0100

    fixone 3mendo

--- src/lib/api/animeworld.ts
+++ src/lib/api/animeworld.ts
@@ -1,4 +1,6 @@
 
+import type { WaitForOptions } from "puppeteer";
+import { encode, decode } from "base64-url";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url); // Viene usato il "require()" per non includere nel bundle un certo modulo
@@ -7,30 +9,37 @@ const puppeteerExtra: typeof import("puppeteer-extra").default = require("puppet
 const { executablePath }: typeof import("puppeteer") = require("puppeteer");
 
 const puppeteer = puppeteerExtra.use(puppeteerExtraPluginStealth());
-const browser = puppeteer.launch({ args: [ '--no-sandbox' ], executablePath: executablePath() });
+const browser = puppeteer.launch({ args: [ '--no-sandbox' ], executablePath: executablePath(), headless: false });
 
+/**
+ * Ottiene il link del flusso video di un episodio da "https://www.animeworld.tv"
+ * @param name Nome della stagione della serie
+ * @param ep Episodio della serie
+ */
 export default async function getAnimeUrl(name: string, ep: string) {
   const page = await (await browser).newPage();
+  const opts: WaitForOptions = { waitUntil: "domcontentloaded" };
 
   try
   {
     // Ricerca serie
     const url = new URL("https://www.animeworld.tv/search");
     url.searchParams.set("keyword", name);
-    await page.goto(url.href);
+
+    // Passaggio per proxy
+    await page.goto(`https://www.hidemyass-freeproxy.com/proxy/en-ww/${ encode(url.href) }?agreed=1`, opts);
 
     // Selezione serie
     const stagione = await page.evaluate(() => (document.querySelector(".film-list > .item a") as HTMLAnchorElement)!.href);
-    await page.goto(stagione);
+    await page.goto(stagione, opts);
 
     // Selezione episodio
     const episodio = await page.evaluate(x => (document.querySelector(`.server.active .episodes.range .episode a[data-episode-num="${ x }"]`) as HTMLAnchorElement)!.href, ep);
-    await page.goto(episodio);
+    await page.goto(episodio, opts);
 
-    // Link di download
-    const link = new URL(await page.evaluate(() => (document.querySelector("#download .widget-body center a") as HTMLAnchorElement)!.href));
-    link.pathname = link.searchParams.get("id")!;
-    link.searchParams.delete("id");
+    // Link di download estratto da quello con il proxy
+    const proxy = await page.evaluate(() => (document.querySelector("#alternativeDownloadLink") as HTMLAnchorElement)!.href);
+    const link = new URL(decode(proxy.match(/([^/]*)$/)[1]));
     return link;
   }
   catch (e) { return console.error(e), null; }
