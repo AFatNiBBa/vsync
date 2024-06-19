e8932aa92f9eb1bbe88e28e91f7dd046064edeed
AFatNiBBa <seanalunni@gmail.com>
2024-06-19 20:03:24 +0200

    Aggiunta la percentuale di vittoria sul calcolatore di roulette Tolto "solid-model"

--- package/vsync/src/route/roulette/(roulette).tsx
+++ package/vsync/src/route/roulette/(roulette).tsx
@@ -5,31 +5,34 @@ import layout from "@seanalunni/style/layout";
 import util from "../../style/util.module.scss";
 
 import { createSignal } from "solid-js";
-import { Atom } from "solid-model";
 
-/** Cifre decimali da mostrare sul contatore della puntata */
-const CIFRE = 2, PRECISION = 10 ** CIFRE;
+/** Funzioni di approssimazione dei vari contatori */
+const APPROX_PUNTATA = createApprox(2, Math.ceil), APPROX_PERC = createApprox(5, Math.floor);
 
 /** Calcolatore di puntate alla roulette col metodo di Fibonacci */
 export default function() {
-	const multiplier = Atom.from(createSignal(.5));
-	const current = Atom.from(createSignal(0));
-	const index = Atom.from(createSignal(0));
-	const puntata = current.convert(x => Math.round(x * multiplier.value * PRECISION) / PRECISION, x => x / multiplier.value);
+	const [ multiplier, setMultiplier ] = createSignal(.5);
+	const [ current, setCurrent ] = createSignal(0);
+	const [ volta, setVolta ] = createSignal(0);
+
+	const puntata = () => APPROX_PUNTATA(current() * multiplier());
+	const perc = () => APPROX_PERC(100 * (1 - (2 / 3) ** volta()));
 	
 	var iter: Iterator<number>;
-	const next = () => (index.value++, current.value = iter.next().value!);
-	const reset = () => (iter = fibonacci(), index.value = 0, next());
+	const next = () => (setVolta(x => x + 1), setCurrent(iter.next().value!));
+	const reset = () => (iter = fibonacci(), setVolta(0), next());
 	reset();
 
 	return <>
 		<div class={`${style.page} ${util.layer} ${layout.center}`}>
 			Importo unitario (€)
-			<Field atom={multiplier} />
+			<input type="number" min={0} step={.1} value={multiplier()} onInput={e => setMultiplier(e.currentTarget.valueAsNumber)} />
 			Puntata corrente (€)
-			<Field readOnly atom={puntata} />
+			<input type="number" readOnly value={puntata()} />
 			Numero puntata
-			<Field readOnly atom={index} />
+			<input type="number" readOnly value={volta()} />
+			Probabilità di vittoria (%)
+			<input type="number" readOnly value={perc()} />
 			<div class={util.control}>
 				<button title="Vittoria" class={`${layout.align} ${color.backSuccess}`} onClick={reset}>
 					<i class="fa-duotone fa-sack-dollar" />
@@ -42,25 +45,20 @@ export default function() {
 	</>
 }
 
-/** Componente che rappresenta un campo da input della pagina */
-function Field(props: { atom: Atom<number>, readOnly?: boolean }) {
-	return <>
-		<input
-			type="number"
-			step={.1}
-			min={0}
-
-			value={props.atom.value}
-			readOnly={props.readOnly}
-			onInput={e => props.atom.value = e.currentTarget.valueAsNumber}
-		/>
-	</>
-}
-
 /** Funzione che genera un'iteratore che emette la sequenza di Fibonacci */
 function *fibonacci() {
 	var prev = 0, last = 1;
 	yield last;
 	for (var curr: number; true; prev = last, last = curr)
 		yield curr = prev + last;
+}
+
+/**
+ * Genera una funzione di approssimazione a {@link cifre} cifre decimali
+ * @param cifre Numero di cifre decimali al quale approssimare
+ * @param f Funzione di arrotondamento
+ */
+function createApprox(cifre: number, f = Math.round) {
+	const mul = 10 ** cifre;
+	return (x: number) => f(x * mul) / mul;
 }
