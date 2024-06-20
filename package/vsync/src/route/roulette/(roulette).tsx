bc2644f87c33eefce3275a74cb58384780e1b27d
s.alunni <sean.alunni@gbsoftware.it>
2024-06-20 09:02:30 +0200

    Messo il contatore della volta corrente in base 1 ed aggiunto lo zero alla sequenza di Fibonacci sul calcolatore di roulette Rinominato in "save" il comando "copy"

--- package/vsync/src/route/roulette/(roulette).tsx
+++ package/vsync/src/route/roulette/(roulette).tsx
@@ -9,6 +9,9 @@ import { createMemo, createSignal } from "solid-js";
 /** Possibilità di vincere per ogni turno */
 const CHANCE = 1 / 3;
 
+/** Valore iniziale del campo volta */
+const DEFAULT_VOLTA = 1;
+
 /** Funzioni di approssimazione dei vari contatori */
 const APPROX_IMPORTO = createApprox(2), APPROX_PERC = createApprox(5);
 
@@ -17,7 +20,7 @@ export default function() {
 	const [ multiplier, setMultiplier ] = createSignal(.4);
 	const [ spesa, setSpesa ] = createSignal(0);
 	const [ saltati, setSaltati ] = createSignal(0);
-	const [ volta, setVolta ] = createSignal(0);
+	const [ volta, setVolta ] = createSignal(DEFAULT_VOLTA);
 
 	const current = createMemo(() => APPROX_IMPORTO(fibonacciAt(saltati() + volta()) * multiplier()));
 	const ricavo = createMemo(() => APPROX_IMPORTO(current() / CHANCE));
@@ -38,11 +41,11 @@ export default function() {
 			Turni iniziali saltati
 			<input type="number" min={0} value={saltati()} onInput={e => setSaltati(e.currentTarget.valueAsNumber)} />
 			Numero puntata
-			<input type="number" readOnly class={`${style.important} ${util.input}`} value={volta() + 1} />
+			<input type="number" readOnly class={`${style.important} ${util.input}`} value={volta()} />
 			Probabilità di vittoria (%)
 			<input type="number" readOnly class={util.input} value={APPROX_PERC(100 * (1 - (1 - CHANCE) ** volta()))} />
 			<div class={util.control}>
-				<button title="Vittoria" class={`${layout.align} ${color.backSuccess}`} onClick={() => setSpesa(setVolta(0))}>
+				<button title="Vittoria" class={`${layout.align} ${color.backSuccess}`} onClick={() => (setSpesa(0), setVolta(DEFAULT_VOLTA))}>
 					<i class="fa-duotone fa-sack-dollar" />
 				</button>
 				<button title="Sconfitta" class={`${layout.align} ${color.backDanger}`} onClick={() => (setSpesa(x => APPROX_IMPORTO(x + current())), setVolta(x => x + 1))}>
@@ -56,6 +59,7 @@ export default function() {
 /** Funzione che genera un'iteratore che emette la sequenza di Fibonacci */
 function *fibonacci() {
 	var prev = 0, last = 1;
+	yield prev;
 	yield last;
 	for (var curr: number; true; prev = last, last = curr)
 		yield curr = prev + last;
