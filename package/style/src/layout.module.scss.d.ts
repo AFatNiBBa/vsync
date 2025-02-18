06a2456d21fb1401a53e65cd6666332a9eaa3fad
Sean Alunni <sean.alunni@gbsoftware.it>
2025-02-18 11:50:49 +0100

    Tolto `layout.host` dai coglioni

--- package/style/src/layout.module.scss.d.ts
+++ package/style/src/layout.module.scss.d.ts
@@ -1,7 +1,6 @@
 declare const layout: {
   readonly scroll: string;
   readonly root: string;
-  readonly host: string;
   readonly slot: string;
   readonly stretch: string;
   readonly center: string;
