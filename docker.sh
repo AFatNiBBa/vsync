3251fdafe8755ba4b91662c689b6df158e59584d
Sean (Server) <seanalunni@gmail.com>
2026-01-01 20:27:01 +0000

    Aggiunta configurazione Docker per Shimmie

---
+++ docker.sh
@@ -0,0 +1,32 @@
+
+# Ottengo il persorso della cartella corrente
+DIR="$(dirname "$(realpath $0)")"
+
+# Crea i volumi esterni per docker
+mkdir -p ~/shimmie ~/public
+chmod 777 ~/shimmie ~/public
+
+# Crea una rete condivisa tra tutti i docker
+docker network create vsync
+
+# Esegui il container di Shimmie
+docker run \
+  --name shimmie \
+  --network vsync \
+  --rm \
+  -d \
+  -v ~/shimmie:/app/data \
+  shish2k/shimmie2:2
+
+# Esegui il container di Nginx
+docker run \
+  --name nginx \
+  --network vsync \
+  --rm \
+  -d \
+  -p 80:80 \
+  -p 443:443 \
+  -v "$DIR/nginx.conf:/etc/nginx/nginx.conf:ro" \
+  -v ~/public:/var/www/static:ro \
+  nginx
+
