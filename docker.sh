c61761b3b6ef947a6a14ff3a90049a5ac4e15e4e
Sean (Server) <seanalunni@gmail.com>
2026-01-02 23:17:18 +0000

    Migrato da Shimmie2 a SzuruBooru

--- docker.sh
+++ docker.sh
@@ -21,10 +21,16 @@ f_start() {
 ######################################################
 
 # Ottengo il persorso della cartella corrente
-DIR="$(dirname "$(realpath $0)")"
+dir="$(dirname "$(realpath $0)")"
+
+# Specifico la configurazione del database di SzuruBooru
+# Il database è esposto solo sulla rete virtuale creata da me, quindi non c'è problema se il mondo conosce la password
+szuru_db_name=szuru
+szuru_db_user=szuru
+szuru_db_pass=szuru
 
 # Crea i volumi esterni per Docker
-mkdir -p ~/data/shimmie ~/public
+mkdir -p ~/data/szuru_db ~/data/szuru_server ~/public
 chmod 777 ~/data ~/public
 
 # Crea una rete condivisa tra tutti i Docker
@@ -34,14 +40,31 @@ f_check "Network" "Created" "Already exists"
 # Esegui il container di Vsync
 f_start vsync vsync
 
-# Esegui il container di Shimmie
-f_start shimmie shish2k/shimmie2:2 \
-  -v ~/data/shimmie:/app/data
+# Esegui il container del database di SzuruBooru
+f_start szuru_db postgres:11-alpine \
+  -e "POSTGRES_DB=$szuru_db_name" \
+  -e "POSTGRES_USER=$szuru_db_user" \
+  -e "POSTGRES_PASSWORD=$szuru_db_pass" \
+  -v ~/data/szuru_db:/var/lib/postgresql/data
+
+# Esegui il container del server di SzuruBooru
+f_start szuru_server szurubooru/server \
+  -e "POSTGRES_HOST=szuru_db" \
+  -e "POSTGRES_DB=$szuru_db_name" \
+  -e "POSTGRES_USER=$szuru_db_user" \
+  -e "POSTGRES_PASSWORD=$szuru_db_pass" \
+  -v "$dir/szuru.yaml:/opt/app/config.yaml" \
+  -v ~/data/szuru_server:/data
+
+# Esegui il container del client di SzuruBooru
+f_start szuru_client szurubooru/client \
+  -e "BACKEND_HOST=szuru_server" \
+  -v ~/data/szuru_server:/data:ro
 
 # Esegui il container di Nginx
 f_start nginx nginx \
   -p 80:80 \
   -p 443:443 \
-  -v "$DIR/nginx.conf:/etc/nginx/nginx.conf:ro" \
+  -v "$dir/nginx.conf:/etc/nginx/nginx.conf:ro" \
   -v ~/public:/var/www/static:ro \
   -v /etc/letsencrypt:/etc/letsencrypt:ro
