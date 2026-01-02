cd8bce21bb2b2c0d6ccb15f62306d6920e2ff49e
Sean (Server) <seanalunni@gmail.com>
2026-01-02 20:32:27 +0000

    Cambiato il sottodominio "shimmie.vsync.it" in "booru.vsync.it"

--- palette.sh
+++ palette.sh
@@ -7,7 +7,7 @@ snap install --classic certbot
 certbot delete --cert-name vsync.it
 
 # Rinnova i certificati adesso E quando scadono
-certbot certonly --webroot -w ~/public -d vsync.it -d shimmie.vsync.it --deploy-hook "docker stop nginx && sh ~/vsync/docker.sh"
+certbot certonly --webroot -w ~/public -d vsync.it -d booru.vsync.it --deploy-hook "docker stop nginx && sh ~/vsync/docker.sh"
 
 # Riavvio Nginx
 docker stop nginx && sh ~/vsync/docker.sh
