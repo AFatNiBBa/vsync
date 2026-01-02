8f97ed2f8a26c7cda0b32d7b2da26359c4b8b480
Sean (Server) <seanalunni@gmail.com>
2026-01-02 19:10:34 +0000

    Corretto script

--- palette.sh
+++ palette.sh
@@ -3,9 +3,8 @@
 apt install snapd
 snap install --classic certbot
 
-# Tolgo i vecchi certificati
+# Tolgo i vecchi certificati (Tutti i domini stanno su un certificato solo)
 certbot delete --cert-name vsync.it
-certbot delete --cert-name shimmie.vsync.it
 
 # Rinnova i certificati adesso E quando scadono
 certbot certonly --webroot -w ~/public -d vsync.it -d shimmie.vsync.it --deploy-hook "docker stop nginx && sh ~/vsync/docker.sh"
